1ae5a8e7e55494da75b78087f04fe1271f67a5d3
yutingrui <yutingrui@tal>
2026-04-26 14:16:10 +0800

    Use SilverMelon233/connect-swift 1.2.3 tag

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "OpenZergCommon", targets: ["OpenZergCommon"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SilverMelon233/connect-swift.git", revision: "1ea40e28da770e05ffce70659c6f378c7d99eb29"),
+        .package(url: "https://github.com/SilverMelon233/connect-swift.git", from: "1.2.3"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.36.1"),
     ],
     targets: [
